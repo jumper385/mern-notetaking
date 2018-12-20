dbaaf0672902086572893dcab129f50147ca343f
Henry Chen <henrychen385@gmail.com>
2018-12-21 07:35:15 +0800

    added a signup route to index

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.json({message:'Welcome to the note-taking app API'})
 });
+
+router.post('/signup', (req,res,next) => {
+  res.json({message: 'POST a new profile'})
+})
 
 module.exports = router;
